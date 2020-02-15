9d95397e0ae376cd4751e35d86f5bc7666c8f8ab
Lord1Luciani <17690123+Lord1Luciani@users.noreply.github.com>
2020-02-16 00:49:49 +0100

    *

--- src/Components/MemberList.js
+++ src/Components/MemberList.js
@@ -85,7 +85,6 @@ function MemberList(props) {
                         </TableRow>
                     </TableHead>
                     <TableBody >
-                        {console.log(groupmembers[id].sort((a, b) => b.stats.messagesCount - a.stats.messagesCount))}
                         {groupmembers[id].sort((a, b) => b.stats.messagesCount - a.stats.messagesCount).slice(page * 10, page * 10 + 10).map((member) => {
                             if (!users[id] || !users[id][member.telegram_id]) {
                                 return (
